dfed3e0770a1d0fa62bb4b5005007f1ef8af5b21
Kevin Wang <kw1017@gmail.com>
2019-04-08 21:46:18 -0700

    remove extra

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,5 +1,4 @@
 module.exports = function(app, db) {
-    // Notes
     app.post('/notes', (req, res) => {
         // Create notes here
         console.log(req.body)
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const MongoClient = require ('mongodb').MongoClient;
 const bodyParser = require ('body-parser');
 const app = express();
 
-// Set Port
 const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }))
